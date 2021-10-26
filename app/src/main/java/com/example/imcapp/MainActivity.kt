63429bfe5574361f6d80f7fd61a073648fffd5ae
pradoRebeca <21193423@CFP127ALUNO.LOCAL>
2021-10-26 16:56:20 -0300

    falta o tools

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,13 +1,29 @@
-package com.example.imcapp
+package com.example.menu_datepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.imcapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cadastro)
+    }
 
-        supportActionBar!!.hide()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.menu_salvar -> (
+                    Toast.makeText(this, "Salvar", Toast.LENGTH_SHORT).show()
+                    )
+        }
+        return true
     }
 }
